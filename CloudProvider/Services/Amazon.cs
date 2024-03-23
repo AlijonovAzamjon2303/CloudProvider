65e704fb47f7cee5b2e71ac14154a56894a27f33
A'zamjon <alijonovrobo@gmail.com>
2024-03-23 06:52:04 +0500

    Add project files.

---
+++ CloudProvider/Services/Amazon.cs
@@ -0,0 +1,32 @@
+﻿
+namespace CloudProvider.Services
+{
+    internal class Amazon : ICloudProvider
+    {
+        public void CreateServer(string region)
+        {
+            // statement
+            Console.WriteLine("Amazaon, Create Server");
+        }
+
+        public string GetCDNAAddress()
+        {
+            return "Amazon Address";
+        }
+
+        public string GetFileName(string name)
+        {
+            return "Amazon, File Name";
+        }
+
+        public List<string> ListServers(string region)
+        {
+            return new List<string>();
+        }
+
+        public void StoreProvider(string name)
+        {
+            Console.WriteLine($"{name} Amazon Store Provider");   
+        }
+    }
+}
---
+++ CloudProvider/Services/DropBox.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CloudProvider.Services
+{
+    internal class DropBox : ICloudProvider
+    {
+        public void CreateServer(string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCDNAAddress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFileName(string name)
+        {
+            return "DropBox Get File Name";
+        }
+
+        public List<string> ListServers(string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreProvider(string name)
+        {
+            Console.WriteLine("DropBox Store Provider");
+        }
+    }
+}
